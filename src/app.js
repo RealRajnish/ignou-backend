b1d4b24dc014bf52ff1f264c520280873fd8d5a2
RealRajnish <rajnishf3@gmail.com>
2023-04-17 20:41:50 +0530

    trying to solve cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors"); //for resolving cors error
 
-app.use(cors());
+app.use(
+  cors({ credentials: true, origin: true, exposedHeaders: ["set-cookie"] })
+);
 
 dotenv.config({ path: "./config.env" });
 

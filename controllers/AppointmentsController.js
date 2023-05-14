6150103ada31712f73ee2c62b8dd865cf2d47a97
RealRajnish <rajnishf3@gmail.com>
2023-05-14 22:19:33 +0530

    admin build updated

--- controllers/AppointmentsController.js
+++ controllers/AppointmentsController.js
@@ -26,14 +26,11 @@ const viewAppointments = async (req, res) => {
       Date: 1,
       _id: 0,
     });
-    // let n = data.length();
-    // let temp;
-    // for (let i = n - 1; i > -1; n--) {
-    //   // Storing the elements of the input array in reverse order
-    //   temp[n - i - 1] = array[i];
-    // }
-    res.send(data);
-    console.log(typeof data);
+    let temp = [...data];
+    temp.reverse();
+   
+    res.send(temp);
+    // console.log(temp);
   } catch (error) {
     console.log(error);
   }
